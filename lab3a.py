23a91fcce661610ed01e1b4543f4b8568b496f6c
Narendra Ayer <nkayer@myseneca.ca>
2025-01-30 23:42:49 -0500

    Initialized repository for lab-3-nkayer

---
+++ lab3a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+
+# Author ID: [seneca_id]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+
+# return_number_value() function
+
+def return_number_value():
+    return 10 + 5  # Example: Adding two integers
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
